117eab802cc5b9b580d69995d202a8db9260cbd1
KASUNHapangama <88395585+KASUNHapangama@users.noreply.github.com>
2026-03-16 13:35:00 +0530

    init

---
+++ src/main/frontend/generated/flow/chunks/chunk-331f7a4afaf1561aa68dd5c91c277a4399f1200d2188d1aea06a81f0c2af0b5f.js
@@ -0,0 +1,10 @@
+import '@vaadin/polymer-legacy-adapter/style-modules.js';
+import '@vaadin/login/src/vaadin-login-form.js';
+import '@vaadin/password-field/src/vaadin-password-field.js';
+import '@vaadin/tooltip/src/vaadin-tooltip.js';
+import '@vaadin/button/src/vaadin-button.js';
+import 'Frontend/generated/jar-resources/buttonFunctions.js';
+import '@vaadin/text-field/src/vaadin-text-field.js';
+import '@vaadin/vertical-layout/src/vaadin-vertical-layout.js';
+import '@vaadin/notification/src/vaadin-notification.js';
+import 'Frontend/generated/jar-resources/flow-component-renderer.js';

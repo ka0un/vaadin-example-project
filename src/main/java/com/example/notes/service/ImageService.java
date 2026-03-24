cee0c17b383c54dffd6dcb47ebc467d21798c80f
Ravindu Kulasooriya <ravinduk01@gmail.com>
2026-03-24 15:50:42 +0530

    Developed the service layer of Image

---
+++ src/main/java/com/example/notes/service/ImageService.java
@@ -0,0 +1,71 @@
+package com.example.notes.service;
+
+import com.example.notes.data.entity.Image;
+import com.example.notes.data.entity.User;
+import com.example.notes.data.repository.ImageRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class ImageService {
+
+    private final ImageRepository imageRepository;
+
+    private final String uploadDir = "uploads/";
+
+    public ImageService(ImageRepository imageRepository) {
+        this.imageRepository = imageRepository;
+    }
+
+    public void saveImage(String fileName, InputStream inputStream, long fileSize, User user) throws IOException {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        inputStream.transferTo(baos);
+        byte[] imageBytes = baos.toByteArray();
+
+        // Extract resolution
+        java.awt.image.BufferedImage bufferedImage = ImageIO.read(new ByteArrayInputStream(imageBytes));
+        int width = bufferedImage.getWidth();
+        int height = bufferedImage.getHeight();
+
+        // Extract format
+        String format = fileName.substring(fileName.lastIndexOf('.') + 1);
+
+        // Save file
+        File directory = new File(uploadDir);
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+        File targetFile = new File(uploadDir + fileName);
+        Files.write(targetFile.toPath(), imageBytes);
+
+        // Save to DB
+        Image image = new Image();
+        image.setFileName(fileName);
+        image.setFilePath(targetFile.getAbsolutePath());
+        image.setFileSize(fileSize);
+        image.setFormat(format);
+        image.setWidth(width);
+        image.setHeight(height);
+        image.setUploadTime(LocalDateTime.now());
+
+        image.setUser(user);
+
+        imageRepository.save(image);
+    }
+
+    public List<Image> getAllImages() {
+        return imageRepository.findAll();
+    }
+
+    public List<Image> getAllImagesByUser(User user) {
+        return imageRepository.findByUser(user);
+    }
+}
